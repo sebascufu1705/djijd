ead533d628c3bef807fb3c51f79af0731638879a
Your Name <sebascufu1705@hotmail.com>
2019-09-05 10:37:40 -0500

    creee el main con sus caractristicas principales

---
+++ Main.js
@@ -0,0 +1,13 @@
+let pantalla;
+function setup(){
+craeteCanvas(500,500);
+}
+function draw(){
+switch(pantalla){
+    case 1:
+        background(255);
+        break;
+    case 2:
+        break;
+}
+}
